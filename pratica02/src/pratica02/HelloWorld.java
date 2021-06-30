771857942b01e70a2980af44b02e7aedc92c9221
VagnerhsFranca <vagnervvn7@gmail.com>
2021-06-30 17:32:50 -0300

    math

--- pratica02/src/pratica02/HelloWorld.java
+++ pratica02/src/pratica02/HelloWorld.java
@@ -1,11 +1,18 @@
 package pratica02;
 
+import java.lang.Math;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Hello World " + (1+1) + "!");
+	
+		System.out.println("3 x 4 = " + (3*4));
+		System.out.println("3 + 4 = " + (3+4));
+		System.out.println("3 - 4 = " + (3-4));
+		System.out.println("3 ^ 4 = " + Math.pow(3,4));
+		System.out.println("3 / 4 = " + (3/4) + "." + (3%4));
 	}
 
 }
